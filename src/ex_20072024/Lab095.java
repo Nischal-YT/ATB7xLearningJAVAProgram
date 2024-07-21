3fb2b8a01c7bd595be0b649cbd4589778671b219
Nischal-YT <nischalnayak05@gmail.com>
2024-07-21 15:03:44 +0530

    20th july - If else conditions and switch

---
+++ src/ex_20072024/Lab095.java
@@ -0,0 +1,22 @@
+package ex_20072024;
+
+public class Lab095 {
+    public static void main(String[] args) {
+        //        int a = 10;
+//                 switch (a) {    --valid
+//
+//        }
+
+        char ch = 'A'; // 65
+        switch (ch) {                 //--valid
+        }
+
+//        boolean b = true;
+//        switch (b) {                //--not valid
+//        }
+
+        long a11 = 30l;
+        switch ((int) a11) {          // --valid
+        }
+    }
+}
