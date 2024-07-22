ced50a76e74072cfce85551d4d51ad792b87c015
Nischal-YT <nischalnayak05@gmail.com>
2024-07-22 18:01:53 +0530

    21st july - Loops and continue - before break

---
+++ src/ex_21072024_Loops/Lab120.java
@@ -0,0 +1,36 @@
+package ex_21072024_Loops;
+
+public class Lab120 {
+    public static void main(String[] args) {
+
+
+
+    // Loop with condition
+    //
+//        for (int i = 1; i <= 10; i++) {
+//
+//        if (i == 5) {
+//            break;
+//        }
+//        System.out.println(i);
+
+
+    //}
+
+    // i = 1 -> 1
+    // i = 2 -> 2
+    // i = 3 -> 3
+    // i = 4 -> 4
+    // i = 5 -> Out of loop
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+            if (i == 5) {
+                break;
+            }
+
+
+
+        }
+}
+}
