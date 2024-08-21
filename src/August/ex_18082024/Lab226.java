0c10b0ffa19f1ccbb484e9ae8d14dd846ba50b0d
Nischal-YT <nischalnayak05@gmail.com>
2024-08-21 11:39:00 +0530

    Exception Handling

---
+++ src/August/ex_18082024/Lab226.java
@@ -0,0 +1,19 @@
+package August.ex_18082024;
+
+public class Lab226 {
+
+    public static void main(String[] args) {
+        String name = "Pramod"; // pramod
+        String name2 = "pramod"; // pramod
+        if (name.equalsIgnoreCase(name2)){
+            System.out.println("Equal");
+        }else {
+            System.out.println("Not EQUAL");
+        }
+
+        // final -> constant
+        // finally -> block of code with the try and catch
+        // finalize -> Object class function - threading use case
+
+    }
+}
