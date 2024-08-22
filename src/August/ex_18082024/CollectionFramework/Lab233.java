73f82f3fad77c2b4f3320191113216e129306e68
Nischal-YT <nischalnayak05@gmail.com>
2024-08-22 11:53:20 +0530

    Collection framework

---
+++ src/August/ex_18082024/CollectionFramework/Lab233.java
@@ -0,0 +1,18 @@
+package August.ex_18082024.CollectionFramework;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lab233 {
+    public static void main(String[] args) {
+        List<Integer> marks = new ArrayList();
+        marks.add(100);
+//        marks.add("pramod");
+//        marks.add(true);
+//        Collection -I
+//        Collections -C
+
+
+    }
+
+}
